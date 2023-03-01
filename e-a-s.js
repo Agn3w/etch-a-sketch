04929c59754817eab7aef013b65e3dca80c4ebc4
Dani <daninovet@proton.me>
2023-03-01 09:09:31 -0600

    write prompt logic, buttons, style

--- e-a-s.js
+++ e-a-s.js
@@ -1,50 +1,46 @@
 let divGrid = document.createElement("div");
-
 let gridContainer = document.querySelector("#grid");
 gridContainer.appendChild(divGrid);
 divGrid.classList.add("gridElement");
 
-function howMany(){
-return prompt("How many units per side do you want?");
-};
 
-let gridInput = howMany()
-let gridInt = parseInt(gridInput);
-let gridSize = (gridInt * gridInt);
-
-
-
-    
-
-for (let i = 1; i < gridSize; i++){
-let item = document.querySelector(".gridElement");
-let newItem = item.cloneNode(true);
-gridContainer.appendChild(newItem);
+function resize(){
+  location.reload();
 }
 
-const pixel = document.querySelector('#grid')
+let input = prompt("How many units per side do you want?");
+let gridInt = parseInt(input);
+let gridSize = (gridInt * gridInt);
 
+  for (let i = 1; i < gridSize; i++){
+    let item = document.querySelector(".gridElement");
+    let newItem = item.cloneNode(true);
+    gridContainer.appendChild(newItem);
+  }
+
+const pixel = document.querySelector('#grid')
 pixel.addEventListener("mouseover", (event) => {
   if(event.target.className === "gridElement") {
-    event.target.style.backgroundColor = "black";
-  }
-})
-
+  event.target.style.backgroundColor = "black";
+    }
+  })
 pixel.addEventListener("click", (event) => {
   if(event.target.className === "gridElement") {
-    event.target.style.backgroundColor = "white";
+  event.target.style.backgroundColor = "white";
   }
 })
+const root = document.querySelector(":root");
+
+root.style.setProperty("--gridSide", gridInt);
+ 
+
+
+  
 
 const resetButton = document.querySelector("#reset")
 
 resetButton.addEventListener("click", function() {
 let ele = document.getElementsByClassName('gridElement');
-for (var i = 0; i < ele.length; i++ ) {
+for (var i = 0; i < ele.length; i++) {
     ele[i].style.backgroundColor = "white";
 }})
-
-const root = document.querySelector(":root");
-
-root.style.setProperty("--gridSide", gridInt);
-
